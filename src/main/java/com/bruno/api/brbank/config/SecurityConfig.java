88d9fb43ecab26fa62b51b31fe39377f84281487
Bruno Silva <devbruninn@outlook.com>
2023-10-19 14:02:22 -0300

    Adding a securityFilterChain method

--- src/main/java/com/bruno/api/brbank/config/SecurityConfig.java
+++ src/main/java/com/bruno/api/brbank/config/SecurityConfig.java
@@ -1,9 +1,24 @@
 package com.bruno.api.brbank.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csfr -> csfr.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
 }
